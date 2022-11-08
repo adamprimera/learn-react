af531168ac8a4f1f73a18ccd73a9129d663538e2
Adam Rachman <adamprmera@gmail.com>
2022-11-08 14:23:32 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className='min-h-screen flex justify-center items-center'>
+      <h1 className={'text-xl'}>Vite + React + Tailwind CSS</h1>
+    </div>
+  )
+}
